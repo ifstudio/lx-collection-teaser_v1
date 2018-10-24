1835a18c194164deb323aed2c50d21a62d4c0c63
Liam Ellis <liamhellis@gmail.com>
2018-10-24 16:40:16 -0400

    refactored client side ui lettering animation, removed client side js files

---
+++ src/components/main/entry.js
@@ -0,0 +1,97 @@
+import anime from 'animejs'
+import charming from 'charming'
+
+/**
+ Inspired By....
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2018, Codrops
+ * http://www.codrops.com
+ */
+
+class Entry {
+  constructor(el, observer) {
+    this.DOM = {
+      el: el
+    }
+    this.DOM.image = this.DOM.el.querySelector('.content__img')
+    this.DOM.title = {
+      word: this.DOM.el.querySelector('.content__text')
+    }
+
+    if (this.DOM.title.word) {
+      charming(this.DOM.title.word)
+      this.DOM.title.letters = Array.from(this.DOM.title.word.querySelectorAll('span'))
+      this.DOM.title.letters.forEach(letter => letter.dataset.initial = letter.innerHTML)
+      this.lettersTotal = this.DOM.title.letters.length
+      observer.observe(this.DOM.el)
+    }
+  }
+  enter(direction = 'down') {
+    this.DOM.title.word.style.opacity = 1
+
+    this.DOM.title.letters.forEach((letter, pos) => {
+      anime.remove(letter)
+      let letterAnim = {
+        targets: letter,
+        duration: 500,
+        delay: () => pos * 80,
+        easing: 'easeOutQuint',
+        opacity: {
+          value: [0, 1],
+          duration: 400,
+          easing: 'linear'
+        }
+      }
+      if (anime.random(0, 1) > 0.5) {
+        letterAnim.translateX = [anime.random(0, 1) > 0.5 ? -window.innerWidth : window.innerWidth, 0]
+      } else {
+        letterAnim.translateY = [anime.random(0, 1) > 0.5 ? -window.innerHeight : window.innerHeight, 0]
+      }
+      anime(letterAnim)
+    })
+
+    anime.remove(this.DOM.image)
+    // anime({
+    //     targets: this.DOM.image,
+    //     duration: 800,
+    //     easing: 'easeOutQuad',
+    //     rotate: () => direction === 'down' ? 5 : -5
+    // });
+  }
+  exit(direction = 'down') {
+    this.DOM.title.letters.forEach((letter, pos) => {
+      anime.remove(letter)
+      let letterAnim = {
+        targets: letter,
+        duration: 500,
+        delay: () => pos * 80,
+        easing: 'easeOutQuint',
+        opacity: {
+          value: 0,
+          duration: 200,
+          easing: 'linear'
+        }
+      }
+      if (anime.random(0, 1) > 0.5) {
+        letterAnim.translateX = [0, anime.random(0, 1) > 0.5 ? -window.innerWidth : window.innerWidth]
+      } else {
+        letterAnim.translateY = [0, anime.random(0, 1) > 0.5 ? -window.innerHeight : window.innerHeight]
+      }
+      anime(letterAnim)
+    })
+
+    anime.remove(this.DOM.image)
+    anime({
+      targets: this.DOM.image,
+      duration: 800,
+      easing: 'easeOutQuad',
+      rotate: 0
+    })
+  }
+}
+
+export default Entry
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -5,6 +5,8 @@ import { TextAndImage } from './TextAndImage'
 import { Logo } from '../svg/logo'
 import { sections } from './sections'
 import { BROWSER } from '../../browser'
+import Entry from './entry'
+import imagesLoaded from 'imagesloaded'
 
 const sum = (a, b) => a + b
 
@@ -13,22 +15,57 @@ class Main extends Component {
     const uncommon = document.getElementById('uncommon')
     const sections = Array.from(document.querySelectorAll('.content__section'))
     const signup = document.querySelector('.sign_up__link')
-    const script1 = document.createElement('script')
-    const script2 = document.createElement('script')
-    const script3 = document.createElement('script')
-    const script4 = document.createElement('script')
-    const body = document.querySelector('body')
+    // const script1 = document.createElement('script')
+    // const script2 = document.createElement('script')
+    // const script3 = document.createElement('script')
+    // const script4 = document.createElement('script')
+    // const body = document.querySelector('body')
 
-    script1.src = '/js/imagesloaded.pkgd.min.js'
-    script2.src = '/js/charming.min.js'
-    script3.src = '/js/anime.min.js'
-    script4.src = '/js/demo4.js'
+    let observer
+    let current = -1
+    let allentries = []
 
-    const scripts = [script1, script2, script3, script4]
+    // Preload all the images in the page..
+    // imagesLoaded(document.querySelectorAll('.content__img'), () => {
+    //   document.body.classList.remove('loading')
 
-    scripts.forEach(script => {
-      body.appendChild(script)
-    })
+    // })
+    if ('IntersectionObserver' in window) {
+      document.body.classList.add('ioapi')
+
+      observer = new IntersectionObserver(
+        entries => {
+          entries.forEach(entry => {
+            if (entry.intersectionRatio > 0.5) {
+              const newcurrent = sections.indexOf(entry.target)
+              if (newcurrent === current) return
+              const direction = newcurrent > current
+              if (current >= 0) {
+                allentries[current].exit(direction ? 'down' : 'up')
+              }
+              allentries[newcurrent].enter(direction ? 'down' : 'up')
+              current = newcurrent
+            }
+          })
+        },
+        {
+          threshold: 0.5
+        }
+      )
+
+      sections.forEach(section => allentries.push(new Entry(section, observer)))
+    }
+
+    // script1.src = '/js/imagesloaded.pkgd.min.js'
+    // script2.src = '/js/charming.min.js'
+    // script3.src = '/js/anime.min.js'
+    // script4.src = '/js/demo4.js'
+
+    // const scripts = [script1, script2, script3, script4]
+
+    // scripts.forEach(script => {
+    //   body.appendChild(script)
+    // })
     sections.length = sections.length - 1
     const distanceToScroll = sections
       .map(section => section.offsetHeight)
